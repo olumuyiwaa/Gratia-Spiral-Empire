51e9271cf748215a9a78d4dd54f099d9fab5e100
olumuyiwaa <oladoyinemmanuel@gmail.com>
2025-11-02 23:39:57 +0100

    favicon change

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Produce a static export in the `out` directory
-  output: "export",
-
-  // Safe even if you aren't using next/image
-  images: { unoptimized: true },
-
-  eslint: { ignoreDuringBuilds: true },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
